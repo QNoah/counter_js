9336ec946151bcd29aad31251719c9ccb15a2b6e
Quinten <quinten.nagel01@gmail.com>
2025-09-08 11:04:03 +0200

    Added an decrease button

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let count = 0;
 
+function increaseCount() {
+  count += 1;
+  document.getElementById("count").textContent = `Count: ${count}`;
+}
 
-document.getElementById('increaseCount').onclick = function(){
-    console.log("werkt");
-    count+= 1;
-    document.getElementById('count').textContent = `Count: ${count}`;
+function decreaseCount() {
+  if (count == 0) {
+    alert("Number can not be lower than 0");
+  } else {
+    count -= 1;
+    document.getElementById("count").textContent = `Count: ${count}`;
+  }
 }
